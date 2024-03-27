0cbdc830d757345a41c3b7f61eb219a35b665457
Alex Cho <alex.jhcho@gmail.com>
2024-03-27 15:06:22 -0700

    feat: add products

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home(request):
-    return render(request, 'store/home.html', {})
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
