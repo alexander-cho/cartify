e9786517cef17d84d9644e52398a620082de496f
Alex Cho <alex.jhcho@gmail.com>
2024-03-28 16:29:05 -0700

    feat: seperate navigation and base template for about page

--- store/views.py
+++ store/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'store/about.html', {})
