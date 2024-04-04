075acb5a1f5d4f726b2f24b30cbe124db5e7a78d
Alex Cho <alex.jhcho@gmail.com>
2024-04-04 16:07:32 -0700

    feat: add dropdown functionality for  quantity selection in the main product display

--- cart/cart.py
+++ cart/cart.py
@@ -23,14 +23,21 @@ class Cart():
         # use ids to look up products in DB model
         products = Product.objects.filter(id__in=product_ids)
         return products
+    
+
+    def get_quantities(self):
+        quantities = self.cart
+        return quantities
 
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_quantity = str(quantity)
+
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'Price:': str(product.price)}
+            self.cart[product_id] = int(product_quantity)
         
         self.session.modified = True
 
--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,8 @@ from django.http import JsonResponse
 def cart_overview(request):
     cart = Cart(request)
     cart_contents = cart.get_cart()
-    return render(request, 'cart/overview.html', {'cart_contents': cart_contents})
+    quantities = cart.get_quantities() # dictionary- {product id: quantity}
+    return render(request, 'cart/overview.html', {'cart_contents': cart_contents, 'quantities': quantities})
 
 
 def add_to_cart(request):
@@ -18,10 +19,12 @@ def add_to_cart(request):
     if request.POST.get('action') == 'post': # action from product page jquery script
         # get the item
         product_id = int(request.POST.get('product_id'))
+        # get the quantity
+        product_quantity = int(request.POST.get('product_quantity'))
         # look up product in DB
         product = get_object_or_404(Product, id=product_id)
         # save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_quantity)
         # get cart quantity
         cart_quantity = cart.__len__()
         # return response
