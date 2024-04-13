f38a37d101063f49551213a4746cbd1a3be0f30d
Alex Cho <alex.jhcho@gmail.com>
2024-04-13 07:09:59 -0700

    feat: migrate Profile model/User model merge, combine for admin

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from django.contrib.auth.models import User
+from .models import Category, Customer, Product, Order, Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+# mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+# extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline] # include everything in the Profile model for the admin area
+
+
+# unregister old format
+admin.site.unregister(User)
+
+# reregister
+admin.site.register(User, UserAdmin)
