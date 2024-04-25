3aacddac69d28143b7adff73022fab15bfbcdd64
Alex Cho <alex.jhcho@gmail.com>
2024-04-24 23:56:45 -0700

    format doc, add ideas for future

--- payment/views.py
+++ payment/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html')
+
+
+def view_previous_orders(request):
+    return '<h1>Previous Orders</h1>'
+
+
+def view_recommendations(request):
+    return '<h1>Recommendations</h1>'
+
+
+def view_other(request):
+    return '<h1>Other</h1>'
