b04fd4300415f86b7197df40ce9bbfdc2cce1ca2
Alex Cho <alex.jhcho@gmail.com>
2024-05-03 21:07:49 -0700

    feat: finish view and component for order checkout

--- payment/views.py
+++ payment/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 
-from store.models import Profile
-from store.forms import UserInfoForm
-from .models import ShippingAddress, Order, OrderItem
+from .models import ShippingAddress
 from .forms import ShippingForm
+
+from cart.cart import Cart
 
 
 # order checkout view
 def checkout(request):
-    order_items = OrderItem.objects.all()
-    shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
-    shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
-    return render(request, 'payment/checkout.html', {'order_items':order_items, 'shipping_form': shipping_form})
+    # Get the cart
+    cart = Cart(request)
+    cart_contents = cart.get_cart()
+    quantities = cart.get_quantities()  # dictionary- {product id: quantity}
+    cart_total = cart.calculate_total()
+
+    if request.user.is_authenticated:
+        # checkout as member
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html',{'cart_contents': cart_contents, 'quantities': quantities, 'cart_total': cart_total, 'shipping_form': shipping_form})
+    else:
+        # checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, 'payment/checkout.html',{'cart_contents': cart_contents, 'quantities': quantities, 'cart_total': cart_total, 'shipping_form': shipping_form})
 
 
 def payment_success(request):
