98457a386e73e922547230a00e899773dca401d8
Alex Cho <alex.jhcho@gmail.com>
2024-05-09 22:41:25 -0700

    feat(payment): add billing info view function, which renders information in billing_info.html depending on the information that user submits in shipping info (previous page), add form to checkout.html to help in handling this

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import ShippingAddress
 from .forms import ShippingForm
@@ -23,6 +24,30 @@ def checkout(request):
         # checkout as guest
         shipping_form = ShippingForm(request.POST or None)
         return render(request, 'payment/checkout.html',{'cart_contents': cart_contents, 'quantities': quantities, 'cart_total': cart_total, 'shipping_form': shipping_form})
+
+
+def billing_info(request):
+    # if user submitted form from previous page (shipping info)
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_contents = cart.get_cart()
+        quantities = cart.get_quantities()  # dictionary- {product id: quantity}
+        cart_total = cart.calculate_total()
+
+        # check to see if user is logged in
+        if request.user.is_authenticated:
+            # shipping_info context is the previous post request that user submitted during checkout
+            return render(request, 'payment/billing_info.html',{'cart_contents': cart_contents, 'quantities': quantities, 'cart_total': cart_total, 'shipping_info': request.POST})
+        else:
+            pass
+
+        shipping_form = request.POST
+
+        return render(request, 'payment/billing_info.html',{'cart_contents': cart_contents, 'quantities': quantities, 'cart_total': cart_total, 'shipping_form': shipping_form})
+    else:
+        messages.success(request, 'Access denied')
+        return redirect('home')
 
 
 def payment_success(request):
